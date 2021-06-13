cc85c48e752950c6ed6adecb508fcb361ad12b36
ahmadalwi425@gmail.com <ahmadalwi425@gmail.com>
2021-06-13 16:20:36 +0700

    update progress

---
+++ resources/views/layouts/partials/setter.blade.php
@@ -0,0 +1,20 @@
+<div id="preloader"></div>
+<a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="{{asset('mainassets/assets/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('mainassets/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('mainassets/assets/vendor/jquery.easing/jquery.easing.min.js')}}"></script>
+<script src="{{asset('mainassets/assets/vendor/php-email-form/validate.js')}}"></script>
+<script src="{{asset('mainassets/assets/vendor/waypoints/jquery.waypoints.min.js')}}"></script>
+<script src="{{asset('mainassets/assets/vendor/counterup/counterup.min.js')}}"></script>
+<script src="{{asset('mainassets/assets/vendor/owl.carousel/owl.carousel.min.js')}}"></script>
+<script src="{{asset('mainassets/assets/vendor/venobox/venobox.min.js')}}"></script>
+<script src="{{asset('mainassets/assets/vendor/aos/aos.js')}}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{asset('mainassets/assets/js/main.js')}}"></script>
+
+</body>
+
+</html>
